81a181dfb87bd9d6dad0f09c417a9b2c94d91435
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2025-04-22 07:37:03 -0400

    Update backPart.js

--- backPart.js
+++ backPart.js
@@ -46,6 +46,7 @@ function theresWinner() {
 
 function confirmRefresh() {
     var okToRefresh = confirm("Do you really want to refresh the page?")
+    
     if (okToRefresh) {
 	setTimeout("location.reload(true);")
     }
